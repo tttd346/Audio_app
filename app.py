c336683ebcb4c71ecb417820022d8ac371de9c34
Robxon <roobsonpl@gmail.com>
2022-10-02 00:32:44 +0200

    initial app file

---
+++ app.py
@@ -0,0 +1,19 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+
+app.layout = html.H1('Hello Dash')
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
