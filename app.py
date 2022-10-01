571b8385781769f338e2d544b9e55b480c4eb5fd
Robxon <roobsonpl@gmail.com>
2022-10-02 00:43:37 +0200

    moved layout to separate file

--- app.py
+++ app.py
@@ -4,15 +4,13 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+from layout import default_layout
 
 app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-
-app.layout = html.H1('Hello Dash')
-
-
+app.layout = default_layout
 
 
 if __name__ == '__main__':
---
+++ layout.py
@@ -0,0 +1,6 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+
+default_layout = html.Div(
+    html.H1('Hello Dash')
+)
